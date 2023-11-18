0f06fb5fea5d2ae8c7ae8009eb00d3985655fa20
avinashp22 <avinashp22@gmail.com>
2023-11-18 20:17:49 +0000

    avinash-week11-com-com-utimateqa-sw-2

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -1,6 +1,34 @@
 package testsuite;
 
 import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginTest extends BaseTest {
+
+    String baseURL = "https://courses.ultimateqa.com/"; // set base url
+
+    @Before
+    public void setUP(){openBrowser(baseURL);}
+
+    @Test
+    public void nameHere1(){
+
+
+    }
+
+
+
+    @Test
+    public void nameHere2(){
+
+
+
+    }
+
+    @After
+    public void tearDown(){closeBrowser();}
+
+
 }
